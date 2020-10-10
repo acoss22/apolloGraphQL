9cc94d44fabcee13231b4547d31896677397d3a5
AnaCS <anacseq@gmail.com>
2020-10-10 13:32:40 +0100

    created client and added react to it

---
+++ client/src/index.tsx
@@ -0,0 +1,39 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  NormalizedCacheObject,
+  ApolloProvider
+} from '@apollo/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pages from './pages';
+import injectStyles from './styles';
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache()
+});
+
+injectStyles();
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+
+
+  // client
+  // .query({
+  //   query: gql`
+  //     query TestQuery {
+  //       launch(id: 56) {
+  //         id
+  //         mission {
+  //           name
+  //         }
+  //       }
+  //     }
+  //   `
+  // })
+  // .then(result => console.log(result));
