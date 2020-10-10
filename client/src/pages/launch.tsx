c4ada7959a07270641414fc697ad26a2039bf7be
AnaCS <anacseq@gmail.com>
2020-10-10 13:48:07 +0100

    client side logic

--- client/src/pages/launch.tsx
+++ client/src/pages/launch.tsx
@@ -1,12 +1,76 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+import { Loading, Header, LaunchDetail } from '../components';
+import { ActionButton } from '../containers';
 import { RouteComponentProps } from '@reach/router';
+import * as LaunchDetailsTypes from './__generated__/LaunchDetails';
+import { LAUNCH_TILE_DATA } from './launches';
 
 interface LaunchProps extends RouteComponentProps {
-
+  launchId?: any;
 }
 
-const Launch: React.FC<LaunchProps> = () => {
-  return <div />;
+
+const Launch: React.FC<LaunchProps>  = ({ launchId }) => {
+  const {
+    data,
+    loading,
+    error
+  } = useQuery<
+    LaunchDetailsTypes.LaunchDetails,
+    LaunchDetailsTypes.LaunchDetailsVariables
+  >(GET_LAUNCH_DETAILS,
+    { variables: { launchId } }
+  );
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR: {error.message}</p>;
+  if (!data) return <p>Not found</p>;
+
+  return (
+    <Fragment>
+      <Header image={data.launch && data.launch.mission && data.launch.mission.missionPatch}>
+        {data && data.launch && data.launch.mission && data.launch.mission.name}
+      </Header>
+      <LaunchDetail {...data.launch} />
+      <ActionButton {...data.launch} />
+    </Fragment>
+  );
 }
+
+
+
+export const GET_LAUNCH_DETAILS = gql`
+  query LaunchDetails($launchId: ID!) {
+    launch(id: $launchId) {
+      id
+      site
+      isBooked
+      rocket {
+        id
+        name
+        type
+      }
+      mission {
+        name
+        missionPatch
+      }
+    }
+  }
+`;
+
+const GET_LAUNCHES = gql`
+  query launchList($after: String) {
+    launches(after: $after) {
+      cursor
+      hasMore
+      launches {
+        ...LaunchTile
+      }
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
 
 export default Launch;
